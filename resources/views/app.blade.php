1707a170d9a33c75144a04e1076d4a077273bc31
ihankhong <hank790315@gmail.com>
2017-11-16 00:13:27 +0800

    調整選單、add logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,25 +13,37 @@
 <body>
 	<nav class="nav navbar-default">
         <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span> 
-                </button>
-                <a class="navbar-brand" href="#">Brand</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li><a data-toggle="tab" href="#home">Home</a></li>
-                    <li><a data-toggle="tab" href="#menu1">Page 1</a></li>
-                    <li><a data-toggle="tab" href="#menu2">Page 2</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i> Log in</a></li>
-                    <li><a href="#">Log out</a></li>
-                    <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Admin</a></li>
-                </ul>
+            <div class="row">
+                <div class="col-xs-12 col-sm-5">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span> 
+                    </button>
+                    <a href="{{ route('home') }}">
+                        <img class="img-responsive" src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}">
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-7">
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <ul class="nav navbar-nav">
+                                    <li><a data-toggle="tab" href="#home">Home</a></li>
+                                    <li><a data-toggle="tab" href="#menu1">Page 1</a></li>
+                                    <li><a data-toggle="tab" href="#menu2">Page 2</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-12">
+                                <ul class="nav navbar-nav">
+                                    <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i> Log in</a></li>
+                                    <li><a href="#">Log out</a></li>
+                                    <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Admin</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
