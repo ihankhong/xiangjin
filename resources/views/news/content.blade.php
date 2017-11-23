6c77a3c158c3ae3c177f61880f85d64403f8b5b3
ihankhong <hank790315@gmail.com>
2017-11-23 21:47:44 +0800

    最新消息 list finish

---
+++ resources/views/news/content.blade.php
@@ -0,0 +1,122 @@
+<section class="tabpanel">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3 col-md-3 col-lg-2">
+				<div class="sidebar">
+					<h4 class="title">最新消息</h4>
+					<ul class="nav nav-pills brand-pills nav-stacked" role="tablist">
+						<li class="brand-nav active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab"><i class="fa fa-play" aria-hidden="true"></i>公司情報</a></li>
+						<li class="brand-nav"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab"><i class="fa fa-play" aria-hidden="true"></i>綜合活動</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sm-9 col-md-9 col-lg-10">
+				<div class="tab-content">
+					<div class="tab-pane active" id="tab1" role="tabpanel">
+						<h2>公司情報</h2>
+						<div class="news-wrap clearfix">
+							<div class="row">
+								<div class="col-xs-12">
+									<div class="wrap clearfix">
+										<div class="col-xs-6 col-sm-9">
+											<h4>2017 SG辦理參展相關活動</h4>
+											<div class="content">
+												<p>HW以為用戶提供壽命長、附加值高的模具為宗旨。通過多年積累的加工技術和延長模具使用壽命方面的豐富經驗，不斷為客戶提供符合其需求的產品。</p>
+											</div>
+										</div>
+										<div class="col-xs-6 col-sm-3">
+											<div class="btns">
+												<button type="button" class="btn btn-primary btn-block">了解更多</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-12">
+									<div class="wrap clearfix">
+										<div class="col-xs-6 col-sm-9">
+											<h4>SG教育訓練規劃活動</h4>
+											<div class="content">
+												<p>HW以為用戶提供壽命長、附加值高的模具為宗旨。通過多年積累的加工技術和延長模具使用壽命方面的豐富經驗，不斷為客戶提供符合其需求的產品。</p>
+											</div>
+										</div>
+										<div class="col-xs-6 col-sm-3">
+											<div class="btns">
+												<button type="button" class="btn btn-primary btn-block">了解更多</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-12">
+									<div class="wrap clearfix">
+										<div class="col-xs-6 col-sm-9">
+											<h4>2017新品發表會</h4>
+											<div class="content">
+												<p>HW以為用戶提供壽命長、附加值高的模具為宗旨。通過多年積累的加工技術和延長模具使用壽命方面的豐富經驗，不斷為客戶提供符合其需求的產品。</p>
+											</div>
+										</div>
+										<div class="col-xs-6 col-sm-3">
+											<div class="btns">
+												<button type="button" class="btn btn-primary btn-block">了解更多</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-12">
+									<div class="wrap clearfix">
+										<div class="col-xs-6 col-sm-9">
+											<h4>2017 SG辦理參展相關活動</h4>
+											<div class="content">
+												<p>HW以為用戶提供壽命長、附加值高的模具為宗旨。通過多年積累的加工技術和延長模具使用壽命方面的豐富經驗，不斷為客戶提供符合其需求的產品。</p>
+											</div>
+										</div>
+										<div class="col-xs-6 col-sm-3">
+											<div class="btns">
+												<button type="button" class="btn btn-primary btn-block">了解更多</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-12">
+									<div class="wrap clearfix">
+										<div class="col-xs-6 col-sm-9">
+											<h4>SG教育訓練規劃活動</h4>
+											<div class="content">
+												<p>HW以為用戶提供壽命長、附加值高的模具為宗旨。通過多年積累的加工技術和延長模具使用壽命方面的豐富經驗，不斷為客戶提供符合其需求的產品。</p>
+											</div>
+										</div>
+										<div class="col-xs-6 col-sm-3">
+											<div class="btns">
+												<button type="button" class="btn btn-primary btn-block">了解更多</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-12">
+									<div class="wrap clearfix">
+										<div class="col-xs-6 col-sm-9">
+											<h4>2017新品發表會</h4>
+											<div class="content">
+												<p>HW以為用戶提供壽命長、附加值高的模具為宗旨。通過多年積累的加工技術和延長模具使用壽命方面的豐富經驗，不斷為客戶提供符合其需求的產品。</p>
+											</div>
+										</div>
+										<div class="col-xs-6 col-sm-3">
+											<div class="btns">
+												<button type="button" class="btn btn-primary btn-block">了解更多</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane" id="tab2" role="tabpanel">
+						<h2>綜合活動</h2>
+						<div class="content">
+							<p>綜合活動內容</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<section>
 		<div class="container-full">
-			<a href="#"><img class="img-responsive center-block" src="{{ asset('images/page/banner/about.jpg') }}" alt=""></a>
+			<a href="#"><img class="img-responsive center-block" src="{{ asset('images/page/banner/new.png') }}" alt=""></a>
 		</div>
 	</section>
 	@include('breadcrumb.breadcrumb')
-	{{-- @include('about.content') --}}
+	@include('news.content')
 @stop
