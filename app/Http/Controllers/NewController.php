0121745ef1c610614acb69974e630691c1fe72e9
ihankhong <hank790315@gmail.com>
2017-11-25 12:12:52 +0800

    最新消息內容頁完成

--- app/Http/Controllers/NewController.php
+++ app/Http/Controllers/NewController.php
@@ -3,21 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\News;
 
 class NewController extends Controller
 {
     public function index()
     {
-    	/**
-    	 * 此sql「select id,title,content from news order by id asc」
-    	 * 與laravel query builder相同
-    	 */
-    	$results = News::select('id','title','content')
-    		->orderBy('id','asc')
-    		->get();
-
-    	return view('news.index',compact('results'));
+    	return view('news.index');
     }
 
     public function article()
--- resources/views/news/article-content.blade.php
+++ resources/views/news/article-content.blade.php
@@ -18,9 +18,10 @@
 							@include('carousel.article-carousel')
 							<div class="article clearfix">
 								<h4>2017榮獲台灣精品獎之肯定</h4>
-								<label for="date">2017.01.01</label>
+								<label class="text-right" for="date">2017.01.01</label>
 								<div class="content">
-									<p></p>
+									<p>東台(4526)今年首度參加9月6日 2017台灣國際3D列印展</p>
+									<p>由經濟部指導、國貿局主辦、中華民國對外貿易發展協會執行的第26屆台灣精品獎結果出爐！東台精機「複合式車削中心VMT-200」、「智能化雷射鑽孔自動化設備TLC-2H22II」、「銑車複合加工中心機TD-1500Y」，以及亞太菁英「動柱棟樑式高速龍門加工中心機MC-18200」四項設備均符合「研發」、「設計」、「品質」、「行銷」等評選之標準，並具備性能優越、操作便利、製程與使用過程綠色環保、以及品質管理等特點獲得評審悄青睞，奪得2018台灣精品獎的殊榮。</p>
 								</div>
 							</div>
 						</div>
@@ -30,6 +31,9 @@
 						<div class="content">
 							<p>綜合活動內容</p>
 						</div>
+					</div>
+					<div class="btns text-right">
+						<a class="back-btn" href="{{ route('news') }}"><img src="{{ asset('images/page/icon/back.png') }}" alt="返回列表">返回列表</a>
 					</div>
 				</div>
 			</div>
